b7a1fbd4cac6663baac00bd417636e71b3329297
Dirk Wiggina <d@irk.com>
2020-07-19 12:03:14 -0400

    logPull

---
+++ logPull.js
@@ -0,0 +1,30 @@
+const Twilio = require('twilio')
+const fs = require('fs')
+
+const client = new Twilio(accountSid, authToken)
+
+// testing setup
+const afterDate = '2020-07-16'
+const beforeDate = '2020-07-17'
+const messageLimit = 1000
+const fullLogFile = 'smsLog.json'
+
+const logMessages = (messages) => {
+  // add full message to the log file
+  fs.appendFile(fullLogFile, JSON.stringify(messages, null, 4), function (err) {
+    if (err) console.log(err)
+  })
+}
+
+const fetchMessages = () => {
+  client.messages
+    .each(
+      {
+        limit: messageLimit,
+        dateSentAfter: afterDate,
+        dateSentBefore: beforeDate
+      }, (messages) => {
+        logMessages(messages)
+      }
+    )
+}
