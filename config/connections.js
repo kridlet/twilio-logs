d5e9f6284c725ffdcc6ac8fb20ee546b60fa72a1
Dirk Wiggina <d@irk.com>
2020-07-19 12:42:24 -0400

    db

--- config/connections.js
+++ config/connections.js
@@ -12,10 +12,35 @@ const connection = mysql.createConnection({
 });
 
 // connect to db
-connection.connect(funtion (err) {
+// Connect to the database
+connection.connect(function (err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
-  
-})
+  connection.query("CREATE DATABASE IF NOT EXISTS twilio_logs", function (err, result) {
+    if (err) {
+      console.error("error creating database: " + err.stack);
+      return;
+    }
+    var sql = "USE twilio_logs;";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        var sql = "CREATE TABLE IF NOT EXISTS smsLogs (id int(11) NOT NULL AUTO_INCREMENT, SID varchar(40) DEFAULT NULL, dateImported datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;";
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          var sql = "select * from smsLogs";
+          connection.query(sql, function (err, result) {
+            if (err) throw err;
+          });
+          console.log("smsLogs table created");
+        });
+      console.log("using twilio_logs");
+    });
+    console.log("twilio_logs database created");
+  });
+  console.log("connected to as id " + connection.threadId);
+});
+
+// Export connection
+module.exports = connection;
