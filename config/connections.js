b3c09463d8d5c5f56f9a5cc8868a5ccf77fafe1c
Dirk Wiggina <d@irk.com>
2020-07-19 14:09:45 -0400

    sql

--- config/connections.js
+++ config/connections.js
@@ -18,7 +18,7 @@ connection.connect(function (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  connection.query("CREATE DATABASE IF NOT EXISTS twilio_logs", function (err, result) {
+  connection.query("CREATE DATABASE IF NOT EXISTS twilioLogs", function (err, result) {
     if (err) {
       console.error("error creating database: " + err.stack);
       return;
@@ -26,18 +26,52 @@ connection.connect(function (err) {
     var sql = "USE twilio_logs;";
     connection.query(sql, function (err, result) {
         if (err) throw err;
-        var sql = "CREATE TABLE IF NOT EXISTS smsLogs (id int(11) NOT NULL AUTO_INCREMENT, SID varchar(40) DEFAULT NULL, dateImported datetime DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;";
+        var sql = `/*
+        Source Server Type    : MySQL
+        Source Host           : masterdb.echo19.com       
+        Target Server Type    : MySQL
+        File Encoding         : utf-8
+        Date: 07/19/2020
+       */
+       
+       SET NAMES utf8mb4;
+       SET FOREIGN_KEY_CHECKS = 0;
+       
+       -- ----------------------------
+       --  Table structure for SmsLog
+       -- ----------------------------
+       DROP TABLE IF EXISTS \`SmsLog\`;
+       CREATE TABLE \`SmsLog\` (
+         \`accountSid\` varchar(32) DEFAULT NULL,
+         \`body\` varchar(1600) DEFAULT NULL,
+         \`dateCreated\` datetime DEFAULT NULL,
+         \`dateSent\` datetime DEFAULT NULL,
+         \`direction\` varchar(10) DEFAULT NULL,
+         \`errorCode\` smallint(6) DEFAULT NULL,
+         \`errorMessage\` varchar(500) DEFAULT NULL,
+         \`from\` varchar(15) DEFAULT NULL,
+         \`messageingServiceSid\` varchar(32) DEFAULT NULL,
+         \`numMedia\` tinyint(4) DEFAULT NULL,
+         \`numSegments\` tinyint(4) DEFAULT NULL,
+         \`price\` decimal(7,5) DEFAULT NULL,
+         \`sid\` varchar(32) DEFAULT NULL,
+         \`status\` varchar(15) DEFAULT NULL,
+         \`to\` varchar(15) DEFAULT NULL
+       ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+       
+       SET FOREIGN_KEY_CHECKS = 1;
+       `;
         connection.query(sql, function (err, result) {
           if (err) throw err;
-          var sql = "select * from smsLogs";
+          var sql = "select * from SmsLog";
           connection.query(sql, function (err, result) {
             if (err) throw err;
           });
-          console.log("smsLogs table created");
+          console.log("SmsLog table created");
         });
-      console.log("using twilio_logs");
+      console.log("using twilioLogs");
     });
-    console.log("twilio_logs database created");
+    console.log("twilioLogs database created");
   });
   console.log("connected to as id " + connection.threadId);
 });
